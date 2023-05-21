fbcfca159d2df441e4492ebdf49cccdb5b3a9f03
MeriemBoujamza <kazahayakun@gmail.com>
2023-05-21 15:48:46 +0200

    final commit

---
+++ app/src/main/java/ma/ensaf/veryempty/models/CUsers.kt
@@ -0,0 +1,50 @@
+package ma.ensaf.veryempty.models
+
+import java.io.Serializable
+
+//this can be deleted now
+//With this annotation we are going to hide compiler warnings
+class CUsers : Serializable {
+    var id = 0
+    var name: String? = null
+    var image = 0
+    var location: String? = null
+    var phoneNumber: String? = null
+    var bloodGroup: String? = null
+    var lastDonatedDate: String? = null
+
+    constructor() {}
+    constructor(
+        id: Int,
+        name: String?,
+        image: Int,
+        location: String?,
+        phoneNumber: String?,
+        bloodGroup: String?
+    ) {
+        this.id = id
+        this.name = name
+        this.image = image
+        this.location = location
+        this.phoneNumber = phoneNumber
+        this.bloodGroup = bloodGroup
+    }
+
+    constructor(
+        id: Int,
+        name: String?,
+        image: Int,
+        location: String?,
+        phoneNumber: String?,
+        bloodGroup: String?,
+        lastDonatedDate: String?
+    ) {
+        this.id = id
+        this.name = name
+        this.image = image
+        this.location = location
+        this.phoneNumber = phoneNumber
+        this.bloodGroup = bloodGroup
+        this.lastDonatedDate = lastDonatedDate
+    }
+}
